09a6a61eeb8bf719791d4050160ef33343b31d41
David Breitenbuecher <dlbreitenbuecher@gmail.com>
2020-08-27 11:49:19 -0700

    create BoxList component, which renders BoxForm and Box components

---
+++ src/BoxList.js
@@ -0,0 +1,49 @@
+import React, {useState} from 'react';
+import Box from './Box';
+import NewBoxForm from './NewBoxForm';
+
+/**Displays all created boxes and new box form
+ * 
+ * Props:
+ * - None
+ * 
+ * State:
+ * -boxes [{backgroundColor, width, height},...]
+ * 
+ * App -> BoxList -> {Box, NewBoxForm}
+ */
+
+ function BoxList() {
+   [boxes, setBoxes] = useState([]);
+
+
+   function addBox(box) {
+     let newBox = { ...box, id: uuid() };
+     setBoxes = [...boxes, newBox];
+   }
+   
+   function renderBoxes(boxes){
+     return(
+      <div>
+      {boxes.map( box =>
+         <Box backgroundColor={box.backgroundColor}
+               width={box.width}
+               height={box.height}
+               key={box.id} />)
+       }
+       </div>
+     )
+
+   }
+
+   return(
+     <div>
+       renderBoxes();
+     </div>
+   )
+ }
+
+
+
+
+export default BoxList
